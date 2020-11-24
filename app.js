452373c81b7fbecb661053153a712cdb291bb456
rodmarrec <romarron01@gmail.com>
2020-11-23 23:56:25 -0600

    sets up files with connection to css file and connection to server

---
+++ app.js
@@ -0,0 +1,22 @@
+// STUB External modules
+const express = require("express");
+
+// STUB instanced modules
+app = express();
+
+// STUB configuration
+const PORT = 3000
+app.set("view engine", "ejs");
+
+// STUB middleware
+app.use(express.static("public"));
+
+// STUB routes
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+// STUB server listener
+app.listen(PORT, () => {
+    console.log(`Listening for client request on ${PORT}`);
+})
